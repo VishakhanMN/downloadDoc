135118291c4b66372fcd17abc2df9c455834f320
Vishakhan MN <32939385+VishakhanMN@users.noreply.github.com>
2026-06-20 16:42:04 +0530

    Create view-document.ts

---
+++ src/app/view-document/view-document.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'view-document',
+  imports: [],
+  templateUrl: './view-document.html',
+  styleUrl: './view-document.css',
+})
+export class ViewDocument {
+
+}
